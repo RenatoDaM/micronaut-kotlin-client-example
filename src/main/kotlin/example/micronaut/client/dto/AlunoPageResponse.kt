578861a34e71831be56ec8686172f5572ba9af84
Renato <renaato.d.m@gmail.com>
2023-11-23 22:12:55 -0300

    patch(AlunoPageResponse): now my get on aluno path return the entire page object, not only the aluno list

---
+++ src/main/kotlin/example/micronaut/client/dto/AlunoPageResponse.kt
@@ -0,0 +1,39 @@
+package example.micronaut.client.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.micronaut.core.annotation.Introspected
+import io.micronaut.serde.annotation.Serdeable
+
+@Introspected
+@Serdeable
+data class ContentResponse(
+    @JsonProperty("content") val content: List<Aluno>,
+    @JsonProperty("pageable") val pageable: Pageable,
+    @JsonProperty("last") val last: Boolean,
+    @JsonProperty("totalPages") val totalPages: Int,
+    @JsonProperty("totalElements") val totalElements: Long,
+    @JsonProperty("size") val size: Int,
+    @JsonProperty("number") val number: Int,
+    @JsonProperty("sort") val sort: Sort,
+    @JsonProperty("first") val first: Boolean,
+    @JsonProperty("numberOfElements") val numberOfElements: Int,
+    @JsonProperty("empty") val empty: Boolean
+)
+@Serdeable
+@Introspected
+data class Pageable(
+    val pageNumber: Int,
+    val pageSize: Int,
+    val sort: Sort,
+    val offset: Int,
+    val paged: Boolean,
+    val unpaged: Boolean
+)
+
+@Introspected
+@Serdeable
+data class Sort(
+    val empty: Boolean,
+    val sorted: Boolean,
+    val unsorted: Boolean
+)
